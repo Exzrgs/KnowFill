ae616bd5aeb50d37b2ef6a95e5bcf42fda3b4ca9
Ayuto <Ayuto1215@outlook.com>
2023-10-07 01:39:56 +0900

    authのapi追加

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from api.views import UserViewSet, ManageUserView, NoteViewSet, ProblemViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register("users", UserViewSet)
@@ -11,5 +12,6 @@ router.register("problem", ProblemViewSet)
 urlpatterns = [
     path("myself/", ManageUserView.as_view(), name="myself"),
     # ユーザ名とパスワードをPOSTするとトークンを返す。
+    path('auth/',obtain_auth_token ),
     path("", include(router.urls)),
 ]
