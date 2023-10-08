ac4ede3f32a87876202eda54e31a6a6d01b10479
Ayuto <Ayuto1215@outlook.com>
2023-10-08 11:50:42 +0900

    トークン認証設定

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
